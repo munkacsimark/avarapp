a70c55aeb3fb0cad636bca5179f6b10b89b94ee0
Mark Munkacsi <munkacsimark@gmail.com>
2018-10-11 04:29:44 +0200

    Category icons added

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -2,12 +2,42 @@
   <view-container>
     <view-header/>
     <div>
-      <router-link
-        v-for="(value, key) in itemCategories"
-        :key="key"
-        :to="`${categoryRouteBase}/${value}`"
-        :class="$style.categoryBox">
-        {{ value }}</router-link>
+      <div :class="$style.contentRow">
+        <router-link
+          :to="beersPath"
+          :class="$style.beerIcon">
+          Sör</router-link>
+        <router-link
+          :to="froccssPath"
+          :class="$style.froccsIcon">
+          Fröccs</router-link>
+      </div>
+      <div :class="$style.contentRow">
+        <router-link
+          :to="hotDrinksPath"
+          :class="$style.hotDrinkIcon">
+          Melegítő</router-link>
+        <router-link
+          :to="softDrinksPath"
+          :class="$style.softDrinkIcon">
+          Üdítő</router-link>
+      </div>
+      <div :class="$style.contentRow">
+        <router-link
+          :to="coffeesPath"
+          :class="$style.coffeeIcon">
+          Kávé</router-link>
+        <router-link
+          :to="winesPath"
+          :class="$style.wineIcon">
+          Bor</router-link>
+      </div>
+      <div :class="$style.contentRow">
+        <router-link
+          :to="snacksPath"
+          :class="$style.snackIcon">
+          Harapnivalók</router-link>
+      </div>
     </div>
     <view-footer/>
   </view-container>
@@ -30,22 +60,63 @@ import { Routes } from '@/router/Routes';
 })
 export default class Categories extends Vue {
 
-  private categoryRouteBase: Routes = Routes.CATEGORIES;
+  private beersPath: string = `${Routes.CATEGORIES}/${ItemCategories.BEER}`;
+  private froccssPath: string = `${Routes.CATEGORIES}/${ItemCategories.FROCCS}`;
+  private softDrinksPath: string = `${Routes.CATEGORIES}/${ItemCategories.SOFT_DRINK}`;
+  private hotDrinksPath: string = `${Routes.CATEGORIES}/${ItemCategories.HOT_DRINK}`;
+  private coffeesPath: string = `${Routes.CATEGORIES}/${ItemCategories.COFFEE}`;
+  private winesPath: string = `${Routes.CATEGORIES}/${ItemCategories.WINE}`;
+  private snacksPath: string = `${Routes.CATEGORIES}/${ItemCategories.SNACK}`;
 
   public constructor() {
     super();
-  }
-
-  get itemCategories(): {[key: string]: any} {
-    return ItemCategories;
   }
 
 }
 </script>
 
 <style lang="scss" module>
-  .categoryBox {
+  @import '../styles/backgrounds';
+  %icon {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-indent: -9999em;
+  }
+  .contentRow {
     display: flex;
-    padding: 10px;
+    justify-content: space-evenly;
+    margin: 30px auto;
+  }
+  .beerIcon {
+    @extend %icon;
+    @include beersBackgroundImage;
+  }
+  .froccsIcon {
+    @extend %icon;
+    @include froccssBackgroundImage;
+  }
+  .hotDrinkIcon {
+    @extend %icon;
+    @include hotDrinksBackgroundImage;
+  }
+  .softDrinkIcon {
+    @extend %icon;
+    @include softDrinksBackgroundImage;
+  }
+  .coffeeIcon {
+    @extend %icon;
+    @include coffeesBackgroundImage;
+  }
+  .wineIcon {
+    @extend %icon;
+    @include winesBackgroundImage;
+  }
+  .snackIcon {
+    @extend %icon;
+    @include snacksBackgroundImage;
   }
 </style>
